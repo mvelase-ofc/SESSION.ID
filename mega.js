0af3fc364c18faa46b97535bc7399caac8ae8735
𝐌𝐕𝐄𝐋𝐀𝐒𝐄-𝐎𝐅𝐂 <mvelaseofc@gmail.com>
2025-04-21 12:18:22 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,7 +3,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'Khulekanimvelase725@gmail.com', // Replace with your Mega email
-    password: 'khustoz24717', // Replace with your Mega password
+    password: 'Khustoz24718', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
